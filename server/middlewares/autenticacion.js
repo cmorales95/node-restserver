14746471bcbfbb885046da86c1a8e198b3bc28dd
cmorales <crisdavidmm95.dev@gmail.com>
2020-07-31 20:50:55 -0500

    Login normal con JWT

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -1,28 +1,40 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 /**
  * Verificar Token
  */
 
- //* Para middlewares usamos next para continuar con la ejecucion del programa
- exports.verificarToken = (req, res, next) => {
-    const token = req.get('Authorization');
+//* Para middlewares usamos next para continuar con la ejecucion del programa
+exports.verificarToken = (req, res, next) => {
+  const token = req.get("Authorization");
 
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    //! Send error
+    if (err) {
+      return res.status(401).json({
+        //* 401 Not Authorized
+        ok: false,
+        err,
+      });
+    }
 
-    jwt.verify( token, process.env.SEED, (err, decoded) => {
-          //! Send error
-          if (err) {
-              return res.status(401).json({ //* 401 Not Authorized
-                  ok: false,
-                  err
-              });
-          }
+    //* Retornamos la informacion del usuario
+    req.usuario = decoded.usuario;
+    next();
+  });
+};
 
-          //* Retornamos la informacion del usuario
-          req.usuario = decoded.usuario;
-          next()
-    })
- };
+exports.verificarAdmin_Role = (req, res, next) => {
+    let usuario = req.usuario
 
-
- 
+    if (usuario.rol === 'ADMIN_ROLE') {
+        next();
+    } else {    
+        return res.status(401).json({
+            ok: false,
+            err: {
+                message: 'El usuario no se administrador'
+            }
+        });
+    }
+};
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const _ = require("underscore");
 
 const Usuario = require("../models/Usuario");
-const { verificarToken } = require("../middlewares/autenticacion");
+const { verificarToken, verificarAdmin_Role } = require("../middlewares/autenticacion");
 // functions
 const sendError = (res, status, error) => {
   return res.status(status).json({
@@ -43,68 +43,74 @@ app.get("/usuario", verificarToken ,function (req, res) {
     });
 });
 
-app.post("/usuario", function (req, res) {
-  const { body } = req;
-  const usuario = new Usuario({
-    nombre: body.nombre,
-    email: body.email,
-    password: bcrypt.hashSync(body.password, 10),
-    rol: body.rol,
-  });
+app.post("/usuario",
+  [verificarToken, verificarAdmin_Role],
+  function (req, res) {
+    const { body } = req;
+    const usuario = new Usuario({
+      nombre: body.nombre,
+      email: body.email,
+      password: bcrypt.hashSync(body.password, 10),
+      rol: body.rol,
+    });
 
-  usuario.save((err, usuarioDB) => {
-    //! Error al guardar
-    if (err) return sendError(res, 200, err);
-
-    //* Guardo correctamente
-    sendJson(res, usuarioDB);
-  });
-});
-
-app.put("/usuario/:id", function (req, res) {
-  const id = req.params.id;
-  const body = _.pick(req.body, ["nombre", "email", "img", "rol", "estado"]);
-
-  Usuario.findByIdAndUpdate(
-    id,
-    body,
-    { new: true, runValidators: true, context: "query" },
-    (err, usuarioDB) => {
+    usuario.save((err, usuarioDB) => {
       //! Error al guardar
       if (err) return sendError(res, 200, err);
 
-      //* Actualizado correctamente
+      //* Guardo correctamente
       sendJson(res, usuarioDB);
-    }
-  );
+    });
 });
 
-app.delete("/usuario/:id", function (req, res) {
+app.put("/usuario/:id",  
+  [verificarToken, verificarAdmin_Role],
+  function (req, res) {
+  
+    const id = req.params.id;
+    const body = _.pick(req.body, ["nombre", "email", "img", "rol", "estado"]);
+
+    Usuario.findByIdAndUpdate(
+      id,
+      body,
+      { new: true, runValidators: true, context: "query" },
+      (err, usuarioDB) => {
+        //! Error al guardar
+        if (err) return sendError(res, 200, err);
+
+        //* Actualizado correctamente
+        sendJson(res, usuarioDB);
+      }
+    );
+});
+
+app.delete("/usuario/:id",  
+  [verificarToken, verificarAdmin_Role],
+  function (req, res) {
   const { id } = req.params;
+    Usuario.findByIdAndUpdate(
+      id,
+      { estado: false },
+      { new: true, context: "query" },
+      (err, usuarioBorrado) => {
+        //! Error al guardar
+        if (err) return sendError(res, 200, err);
 
-  Usuario.findByIdAndUpdate(
-    id,
-    { estado: false },
-    { new: true, context: "query" },
-    (err, usuarioBorrado) => {
-      //! Error al guardar
-      if (err) return sendError(res, 200, err);
+        //* Actualizado correctamente
+        sendJson(res, usuarioBorrado);
+      }
+    );
 
-      //* Actualizado correctamente
-      sendJson(res, usuarioBorrado);
-    }
-  );
+    // Eliminacion fisica desactivada
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    //   //! Error al eliminar
+    //   if (err) return sendError(res, 200, err);
 
-  // Eliminacion fisica desactivada
-  // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
-  //   //! Error al eliminar
-  //   if (err) return sendError(res, 200, err);
+    //   //! Alerta de usuario no encontrado
+    //   if (usuarioBorrado === null) return sendError(res, 200, 'Usuario no encontrado');
 
-  //   //! Alerta de usuario no encontrado
-  //   if (usuarioBorrado === null) return sendError(res, 200, 'Usuario no encontrado');
-
-  //   sendJson(res, usuarioBorrado);
-  // });
+    //   sendJson(res, usuarioBorrado);
+    // });
 });
 
 module.exports = app;
